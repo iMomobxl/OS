092fe844797ec3b2e5b4864487046d623f758f83
iMomobxl <mohamed.alibacha@gmail.com>
2025-01-31 12:20:41 +0100

    script removeuser.sh done

--- tp_script/ex_005/removeuser.sh
+++ tp_script/ex_005/removeuser.sh
@@ -7,3 +7,23 @@ if [[ $EUID -ne 0 ]] ; then
     echo "$sn: Vous devez etre root"
     exit 1
 fi
+
+# check le nombre de parametre (min/max 1)
+if [[ $# -ne 1 ]] ; then
+    echo "Usage: $sn USERNAME"
+    exit 2
+fi
+
+username=$1
+
+# check if the parameter is a user
+getent passwd $username &>/dev/null # getent return 0 if user exist, 1 if user not exist
+
+if [[ $? -ne 0 ]] ; then
+    echo "$sn: L'utilisateur '$username' n'existe pas"
+    exit 3
+fi
+
+userdel -r -f $username &>/dev/bull
+
+exit 0
