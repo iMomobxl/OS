9208060503bcca29ec28d90bfd7d079fba266711
iMomobxl <mohamed.alibacha@gmail.com>
2025-01-31 13:56:36 +0100

    script examen.sh create but not finish

--- tp_script/ex_005/examen.sh
+++ tp_script/ex_005/examen.sh
@@ -7,3 +7,22 @@ if [[ $EUID -ne 0 ]] ; then
     echo "$sn: Vous devez etre root"
     exit 1
 fi
+
+usage="Usaage: '$sn'"
+
+# check si 1 parametre
+if [[ $# -ne 1 ]] ; then
+    echo "$usage [USERNAME]"
+    exit 2
+fi
+
+mkdir /examen
+
+# check si l'utilisateur existe
+username=$1
+
+getent passwd $username &>/dev/null
+
+if [[ $? -ne 1 ]] ; then
+    echo "l'utilisateur n'existe pas"
+    ./newuser $username
